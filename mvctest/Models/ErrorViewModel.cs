f8cf4f4b03e99ad243aed637e7cb98588e789ed0
Federico Rodriguez <Federico.Rodriguez@cudirect.com>
2018-08-21 11:46:36 -0700

    created

---
+++ mvctest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pivotal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
